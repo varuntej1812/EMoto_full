ca1c0625102294a7f5c1b02c24ce1e0871cf0878
varuntej1812 <varuntej.p21@iiits.in>
2025-02-23 18:16:32 +0530

    update

--- backend/src/models/Dashboard.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DashboardSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  data: { type: Object, required: true },
-});
-
-module.exports = mongoose.model("Dashboard", DashboardSchema);
